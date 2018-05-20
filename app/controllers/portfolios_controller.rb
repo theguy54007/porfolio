e933822966001d9255aee14b8d317216b915889d
Tommy <sage.ts920126@gmail.com>
2018-05-20 17:39:07 +0800

    Refactored portfolio find params code in controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @portfolio_items = Portfolio.all    
@@ -9,7 +11,7 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
+    
   end
 
   def new
@@ -31,11 +33,11 @@ class PortfoliosController < ApplicationController
 
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
+    
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
+    
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
@@ -47,7 +49,7 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
+    
     @portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully destroyed.' }
@@ -60,8 +62,15 @@ class PortfoliosController < ApplicationController
   private
 
   def portfolio_params
-      params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
+      params.require(:portfolio).permit(:title, 
+                                        :subtitle, 
+                                        :body, 
+                                        technologies_attributes: [:name]
+                                        )
   end
 
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
 end
