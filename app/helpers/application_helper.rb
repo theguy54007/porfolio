72ea8c58c5426fb353c08d720c3bdbea8a6ec752
Tommy <sage.ts920126@gmail.com>
2018-05-29 12:58:58 +0800

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -61,5 +61,21 @@ module ApplicationHelper
    "active" if current_page? path 
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+       alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title:  "Tommy :)", sticky: false) 
+  end
+
+
+
+
+
 
 end
